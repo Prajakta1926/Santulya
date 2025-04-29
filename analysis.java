b5e84f3d6bc2fc275038aa4ad9738d620bfa2ebd
Prajakta1926 <prajaktadeshpande0014@gmail.com>
2025-04-29 22:26:28 +0530

    Add files via upload

---
+++ analysis.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class JavaCode;
+public static void main(String args[]){
+    System.out.println("Enter the values of link lengths AB(a),BC(b),CD(c),AD(d) respectively.");
+    System.out.println("Input the values of angles beta,theta and phi respectively");
+    System.out.println("Enter the angular velocity omega_a");
+    omega_b=[a*omega_a*sin(phi-theta)]/[b*sin(phi-beta)];
+    System.out.println("angular velocity of link b="omega_b);
+    omega_c=[a*omega_a*sin(beta-theta)]/c*sin(beta-phi);
+    System.out.println("angular velocity of link c="omega_c);
+}
